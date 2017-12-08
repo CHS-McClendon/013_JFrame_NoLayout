fd378055616e3e2beac188877047ac13dda45b51
CHS-McClendon <mcclendon.mike@westada.org>
2017-12-08 10:51:49 -0700

    First push code just started not working yet

---
+++ src/RunHere.java
@@ -0,0 +1,13 @@
+
+public class RunHere {
+
+	public static void main(String[] args) {
+
+		createAndShowGUI();
+	}
+
+	private static void createAndShowGUI() {
+		// TODO Auto-generated method stub
+
+	}
+}
